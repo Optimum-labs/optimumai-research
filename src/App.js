1da9dcfd8cf56d3e21a8896585f6b90ab174f3a0
ajay-dhangar <ajaydhangar49@gmail.com>
2024-04-18 12:01:06 +0530

    added new update

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/optimumai-research/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
       
